bcb3653cd1348100b2b1e991670533cea05a0aff
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-11-12 16:55:27 +0300

    add all posts page

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -1,7 +1,7 @@
 import PostItem from './post-item';
 import classes from '../../styles/posts-grid.module.css';
 
-function PostGrid(props) {
+function PostsGrid(props) {
   const { posts } = props;
   return (
     <ul className={classes.grid}>
@@ -12,4 +12,4 @@ function PostGrid(props) {
   );
 }
 
-export default PostGrid;
+export default PostsGrid;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,8 @@
+import AllPosts from '../../components/posts/all-posts';
+import { DUMMY_POSTS } from '../../lib/dummy-data';
+
 function AllPostsPage() {
-  return <div></div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
 
 export default AllPostsPage;
