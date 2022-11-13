8c721e3a0a3d3ec88cc6defc0f40b43e058f3fc7
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-11-13 23:12:49 +0300

    fix all posts error

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,7 @@
 import AllPosts from '../../components/posts/all-posts';
 import { getAllPosts } from '../../lib/posts-util';
 import Head from 'next/head';
+import { Fragment } from 'react';
 
 function AllPostsPage(props) {
   return (
