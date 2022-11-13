4c4cb6078d27996f53e83c16ded1dbb34b25c28e
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-11-13 23:02:12 +0300

    add head data

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,18 @@ import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
 import { getFeaturedPosts } from '../lib/posts-util';
+import Head from 'next/head';
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>blog - ilkyaz</title>
+        <meta
+          name="description"
+          content="I post about programming and web development"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,20 @@
 import AllPosts from '../../components/posts/all-posts';
 import { getAllPosts } from '../../lib/posts-util';
+import Head from 'next/head';
 
 function AllPostsPage(props) {
-  return <AllPosts posts={props.allPosts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All of my posts</title>
+        <meta
+          name="description"
+          content="A list of all programming related blog posts"
+        />
+      </Head>
+      <AllPosts posts={props.allPosts} />
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
